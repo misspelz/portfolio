cff5533003ab817a651db79e79073923ed2d75f9
Oluwapelumi <pelumiiadetoye@gmail.com>
2023-01-25 00:50:49 +0100

    my portfolio website

--- js/index.js
+++ js/index.js
@@ -4,5 +4,10 @@ const menu = document.getElementById("menu")
 
 bar.addEventListener("click", ()=>{
     menu.classList.toggle("hidden")
-
+    // if(menu.style.display = "hidden"){
+    //     menu.style.display = "block"
+    // }else{
+    //     menu.style.display = "hidden"
+    // }
 })
+
